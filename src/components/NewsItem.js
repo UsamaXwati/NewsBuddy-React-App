9f34e67bafa4935b7340e46d44dc908109665052
UsamaXwati <usamaxwati1@gmail.com>
2023-01-01 23:32:45 +0500

    N-comp

---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        Item of NewsComponent
+      </div>
+    )
+  }
+}
+
+export default NewsItem
