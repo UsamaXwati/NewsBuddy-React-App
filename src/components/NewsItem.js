53fa14c4906305566b35c17b80459d512e01428e
UsamaXwati <usamaxwati1@gmail.com>
2023-01-04 05:44:01 +0500

    some changes

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
          <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..." />
+            <img src={imageUrl?imageUrl:"https://images.hindustantimes.com/tech/img/2023/01/03/1600x900/TOPSHOT-SKOREA-SPACE-MOON-0_1672768347953_1672768347953_1672768377156_1672768377156.jpg"} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}...</p>
-                <a href={newsUrl} className="btn btn-sm btn-primary">Read More</a>
+                <a href={newsUrl} className="btn btn-sm btn-dark">Read More</a>
             </div>
          </div>
         </div>
