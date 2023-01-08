3198496b457f11b61fcf8b9e00e93ff17e8b0d63
UsamaXwati <usamaxwati1@gmail.com>
2023-01-08 21:02:28 +0500

    router-dom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
 import PropTypes from 'prop-types'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 export default class App extends Component {
   static defaultProps = {
@@ -18,8 +24,20 @@ export default class App extends Component {
   render() {
     return (
       <div>
-          <Navbar/>
-          <News pageSize={5} country="in" category="science"/>
+          <Router>
+           <Navbar/>
+          <Routes>          
+            <Route path="/" element={<News pageSize={5} country="in" category="general"/>} />
+            <Route path="business" element={<News pageSize={5} country="in" category="business"/>} />
+            <Route path="entertainment" element={<News pageSize={5} country="in" category="entertainment"/>} />          
+            <Route path="health" element={<News pageSize={5} country="in" category="health"/>} />
+            <Route path="science" element={<News pageSize={5} country="in" category="science"/>} />
+            <Route path="sports" element={<News pageSize={5} country="in" category="sports"/>} />
+            <Route path="technology" element={<News pageSize={5} country="in" category="technology"/>} />
+
+          </Routes>
+            
+          </Router>
       </div>
     )
   }
