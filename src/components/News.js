5ac035507a7e80bb4ad6eb7621f1be0ef4292ffb
UsamaXwati <usamaxwati1@gmail.com>
2023-01-28 23:52:09 +0500

    refi

--- src/components/News.js
+++ src/components/News.js
@@ -30,7 +30,7 @@ const News = (props) => {
     }
     
     useEffect(() => {
-
+        document.title = `${capitalizeFirstLetter(props.category)} - NewsBuddy`;
         updateNews()    
       
     }, [])
